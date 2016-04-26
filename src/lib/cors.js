1e25615c39fc6cdf506629248d645204fd274c47
alphakevin <antmary@126.com>
2016-04-26 16:09:15 +0800

    fix CORS problem

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -1,8 +1,11 @@
 export default (req, res, next) => {
   res.set({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'HEAD,GET,POST,PUT,DELETE,OPTIONS'
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'Access-Control-Allow-Methods' : 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Credentials': true,
+    'X-Content-Type-Options': 'nosniff',
+    'Vary': 'Accept-Encoding',
   });
   if (req.method == 'OPTIONS') {
     res.send();
