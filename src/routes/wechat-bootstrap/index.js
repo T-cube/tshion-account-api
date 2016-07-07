bda71125b1074865121d867f074e5b00678f2d11
j.deng <j.deng@yumeng.co>
2016-07-07 18:03:49 +0800

    update attend doc

--- src/routes/wechat-bootstrap/index.js
+++ src/routes/wechat-bootstrap/index.js
@@ -1,21 +1,22 @@
 import _ from 'underscore';
 import express from 'express';
 import wechat from 'wechat';
+import config from 'config';
 
 import wUtil from 'lib/wechat-util.js';
 
 let api = express.Router();
 export default api;
 
-var config = {
-  token: 'wechat',
-  appid: 'wx0215f16935043abf',
-  encodingAESKey: 'PuUMhTzz0JxYxV7bGw4aeNJdxXq3CIw2cTRVe56cTgP'
+const wechatConfig = {
+  token: config.get('wechat.token'),
+  appid: config.get('wechat.appid'),
+  encodingAESKey: config.get('wechat.encodingAESKey'),
 };
 
 api.use(express.query());
 
-api.use('/', wechat(config, function (req, res) {
+api.use('/', wechat(wechatConfig, function (req, res) {
   let message = req.weixin;
   switch (message.MsgType) {
   case 'event': {
