a1713818969d42a269073677ee6e18210dacbc6f
j.deng <j.deng@yumeng.co>
2016-08-04 13:41:56 +0800

    fix bug

--- src/routes/company/attendance/index.js
+++ src/routes/company/attendance/index.js
@@ -38,7 +38,7 @@ api.post('/sign', ensureFetchSettingOpened, (req, res, next) => {
   checkUserLocation(req.company._id, req.user._id).then(isValid => {
     let data = req.body;
     sanitizeValidateObject(signSanitization, signValidation, data);
-    let from_pc = !!req.query.from_pc && getClientIp();
+    let from_pc = !!req.query.from_pc && getClientIp(req);
     let now = new Date();
     _.extend(data, {
       date: now
