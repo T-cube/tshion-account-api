941a828324f08a1fa71ef10665958c69a10af478
j.deng <j.deng@yumeng.co>
2016-09-02 16:19:34 +0800

    add tcli to update document.file cdn_key

--- src/cli/tcli.js
+++ src/cli/tcli.js
@@ -10,6 +10,7 @@ program
   .command('attendancesign', 'upgrade attendance data structure')
   .command('wechatmenu', 'update wechat menu')
   .command('cdnupgrade', 'upload local files to qiniu cdn & update database')
+  .command('filecdnkey', 'add cdn key to document file')
   .parse(process.argv);
 
 console.log('TLifang API Command Line Tool');
