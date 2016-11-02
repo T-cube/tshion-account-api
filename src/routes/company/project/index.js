7b91e8fbedd2342747c100d66b2c5864d4ba863d
j.deng <j.deng@yumeng.co>
2016-11-02 14:30:16 +0800

    fix bug

--- src/routes/company/project/index.js
+++ src/routes/company/project/index.js
@@ -633,7 +633,7 @@ function ensureProjectOwner(project, user_id) {
 }
 
 function ensureProjectMember(project, user_id) {
-  if (_.find(project.members, member => member._id.equals(user_id))) {
+  if (!_.find(project.members, member => member._id.equals(user_id))) {
     throw new ApiError(400, 'not_project_member');
   }
 }
