b54c70785534df51951590b931fc55508c2b8730
j.deng <j.deng@yumeng.co>
2016-12-13 17:41:33 +0800

    fix bug

--- src/rpc/schema/index.js
+++ src/rpc/schema/index.js
@@ -7,13 +7,13 @@ const schema = {
       _id: { type: 'int', optional: true },
       name: { type: 'string', minLength: 1, rules: ['trim', 'title'] },
       description: { type: 'string', optional: true },
-      status: { type: 'string' },
+      status: { type: 'string', optional: true },
     },
     validation: {
       _id: { type: 'int', optional: true },
       name: { type: 'string' },
       description: { type: 'string', optional: true },
-      status: { $enum: ['deleted', 'normal'] },
+      status: { $enum: ['deleted', 'normal'], optional: true },
     }
   },
   plan_audit: {
