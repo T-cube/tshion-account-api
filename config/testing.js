42edc82894562846bf522b736fa5ccbed882efb1
j.deng <j.deng@yumeng.co>
2016-12-14 14:05:42 +0800

    update testing conf

--- config/testing.js
+++ config/testing.js
@@ -1,8 +1,8 @@
 module.exports = {
-  apiUrl: 'http://api.tlf.test.local/',
-  webUrl: 'http://web.tlf.test.local/',
+  apiUrl: 'http://api.tlf.test.tlifang.cn/',
+  webUrl: 'http://web.tlf.test.tlifang.cn/',
   database: '127.0.0.1/tlf_core',
   upload: {
-    url: 'http://api.tlf.test.local/cdn/',
+    url: 'http://api.tlf.test.tlifang.cn/cdn/',
   },
 };
