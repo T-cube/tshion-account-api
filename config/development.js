a2662ef47856438f240e9cfc9d627a236bbf1ef7
alphakevin <antmary@126.com>
2016-12-16 09:31:59 +0800

    fix package version

--- config/development.js
+++ config/development.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiUrl: 'http://web.tlf.alvin.local/',
-  webUrl: 'http://web.tlf.alvin.local/',
+  apiUrl: 'http://web.tlf.alvin.tlifang.cn/',
+  webUrl: 'http://web.tlf.alvin.tlifang.cn/',
   database: '192.168.1.18/tlf_core',
   upload: {
-    url: 'http://web.tlf.test.local/cdn/',
+    url: 'http://web.tlf.test.tlifang.cn/cdn/',
   },
   vendor: {
     officeweb365: {
