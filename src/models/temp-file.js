d46ec4337e2cdb48097841a895397713859ee1b0
j.deng <j.deng@yumeng.co>
2017-01-03 14:55:28 +0800

    fix bug

--- src/models/temp-file.js
+++ src/models/temp-file.js
@@ -1,46 +1,46 @@
+import _ from 'underscore';
 import { ObjectId } from 'mongodb';
 
 import db from 'lib/database';
 import C from 'lib/constants';
 import {indexObjectId} from 'lib/utils';
 
-export default class AuthPic {
+const qiniu = this.model('qiniu').bucket('cdn-private');
 
-  constructor() {}
+export default class TempFile {
 
-  save({plan, user_id, company_id, files}) {
-    let criteria;
-    if (plan == C.TEAMPLAN.PRO) {
-      if (user_id) {
-        criteria = {user_id};
-      }
-    } else if (plan == C.TEAMPLAN.ENT) {
-      if (company_id) {
-        criteria = {company_id};
-      }
-    }
-    if (!criteria) {
-      throw new Error('invalid params');
-    }
-    return this._save(criteria, files);
+  constructor() {
+    this.db = db.temp.file;
   }
 
-  _save(query, files) {
-    files.forEach(file => file._id = ObjectId());
+  save({files, info, is_private = true, clear_prev = false}) {
+    let query = Object.assign(info, {is_private});
     if (!files || !files.length) {
       return Promise.resolve([]);
     }
-    return db.plan.auth.pic.findAndModify({
-      query,
-      update: {$set: {files}},
-      upsert: true
-    })
-    .then(doc => {
-      let oldFiles = doc.value && doc.value.files;
-      if (oldFiles && oldFiles.length) {
-        // TODO unlink temp files
+    let promise;
+    files.forEach(file => file._id = ObjectId());
+    if (clear_prev) {
+      promise = this.db.findAndModify({
+        query,
+        update: {$set: {files}},
+        upsert: true
+      })
+      .then(doc => {
+        let oldFiles = doc.value && doc.value.files;
+        if (oldFiles && oldFiles.length) {
+          // TODO unlink temp files
+        }
+      });
+    } else {
+      promise = this.db.update(query, {
+        $push: {files}
+      });
+    }
+    return promise.then(() => {
+      if (!is_private) {
+        return files.map(file => _.pick(file, '_id', 'url'));
       }
-      const qiniu = this.model('qiniu').bucket('cdn-private');
       return Promise.all(files.map(file => {
         return qiniu.makeLink(file.cdn_key).then(link => {
           return {
