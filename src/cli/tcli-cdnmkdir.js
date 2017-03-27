67bd321d66d9b97cc67e02126bce4e843f0b5dc5
gea <rightjiang@hotmail.com>
2017-03-27 17:05:50 +0800

    mkdir by files head 3 words

---
+++ src/cli/tcli-cdnmkdir.js
@@ -0,0 +1,49 @@
+import { ObjectId } from 'mongodb';
+import Promise from 'bluebird';
+import xlsx from 'node-xlsx';
+import fs from 'fs';
+import _ from 'underscore';
+import mkdirp from 'mkdirp-bluebird';
+
+import config from 'config';
+import '../bootstrap';
+import program from 'commander';
+import db from 'lib/database';
+import approvalTemplate from './config/approval-template';
+import planData from './config/plan';
+import oauthClient from './config/oauth-clients';
+import { handleError } from './lib/utils';
+
+
+function testLink() {
+  let dir = `${__dirname}/../../public/cdn/upload/attachment`;
+  let files = [];
+  try{
+    files = fs.readdirSync(dir);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+  return Promise.map(files, (file) => {
+    let first = file[0];
+    let second = file[1];
+    let third = file[2];
+    console.log(`${dir}/${first}/${second}/${third}`);
+    return mkdirp(`${dir}/${first}/${second}/${third}`).then(made => {
+      console.log(made);
+      return made;
+    });
+  });
+}
+
+program
+  .command('test-current')
+  .description('read current dir & files')
+  .action(() => {
+    testLink()
+    .then(() => {
+      process.exit(0);
+    })
+    .catch(handleError);
+  });
+
+program.parse(process.argv);
