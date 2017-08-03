b686256243970907a65ae35ffe12463ac25f95f9
gea <rightjiang@hotmail.com>
2017-08-03 16:18:00 +0800

    fix cors

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -1,24 +1,21 @@
 import config from 'config';
+import cors from 'cors';
 
 const allowedOrigins = config.get('allowedOrigins');
 
-export default function cors(req,res,next){
-  let _origin = req.headers.origin,
-    can_next = ~allowedOrigins.indexOf(_origin),
-    Credentials = true;
-  if(allowedOrigins==='*'){
-    can_next = true;
-    Credentials = false;
-    _origin ='*';
-  }
-  res.set('Access-Control-Allow-Origin', _origin);
-  res.set('Access-Control-Allow-Credentials',  Credentials);
-  res.set('Access-Control-Allow-Methods',  'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-
-  if(can_next){
-    next();
-  }else{
-    res.set('Content-Length' , 0);
-    res.status(204).end();
-  }
-}
+export default cors({
+  origin: function(url,callback){
+    let corsOptions;
+    if (~allowedOrigins.indexOf(url)) {
+      corsOptions = {
+        origin: true
+      };
+    } else {
+      corsOptions = {
+        origin: false
+      };
+    }
+    callback(null, corsOptions);
+  },
+  optionsSuccessStatus: 204
+});
