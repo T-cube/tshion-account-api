2fa7373407a7174548b6b42410587e6f54b3b289
gea <rightjiang@hotmail.com>
2017-08-18 11:49:56 +0800

    invitation url error description

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -346,7 +346,7 @@ api.post('/invitation', (req, res, next) => {
   if (check == req.body.h) {
     let now = new Date().getTime();
     if (now - parseInt(t) > 1800000) {
-      res.redirect(301, config.get('webUrl'));
+      throw new ApiError(400, 'invitation_url_expire');
     }
     let company_id = ObjectId(c);
     let url = config.get('webUrl') + `/oa/company/${c}/home`;
