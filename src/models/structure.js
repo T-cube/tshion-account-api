cea5625abe88267835a02524da893718ada8e352
gea <rightjiang@hotmail.com>
2017-09-01 17:25:13 +0800

    wrong quote

--- src/models/structure.js
+++ src/models/structure.js
@@ -317,7 +317,7 @@ class Structure {
     if (!_.some(node.positions, item => item._id.equals(position))) {
       return null;
     }
-    return _.some(node.members, m => (m._id.equals(member_id)&&(m.position = position))) ? {_id: member_id, position} : null;    
+    return _.some(node.members, m => (m._id.equals(member_id))&&(m.position = position)) ? {_id: member_id, position} : null;    
   }
 
   findMemberDepartments(member_id, node, path) {
