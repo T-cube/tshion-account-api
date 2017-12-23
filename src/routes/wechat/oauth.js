badc08c5078ccd7a62763f0262e5d1fb49104db9
gea <rightjiang@hotmail.com>
2017-12-23 13:39:15 +0800

    test wechat oauth

--- src/routes/wechat/oauth.js
+++ src/routes/wechat/oauth.js
@@ -4,10 +4,21 @@ import wechatOAuth from 'wechat-oauth';
 import Promise from 'bluebird';
 import config from 'config';
 import bodyParser from 'body-parser';
+import moment from 'moment';
 
 import C from 'lib/constants';
 import WechatOAuthModel from 'lib/wechat-oauth-model.js';
 import { oauthCheck } from 'lib/middleware';
+import db from 'lib/database';
+import {
+  comparePassword,
+  hashPassword,
+  isMobile,
+  isEmail,
+  generateToken
+} from 'lib/utils';
+import { ApiError } from 'lib/error';
+import { randomAvatar } from 'lib/upload';
 
 const api = express.Router();
 export default api;
@@ -47,13 +58,15 @@ api.get('/entry', (req, res) => {
 
 api.get('/access', access);
 
-api.post('/bind', bodyParser.json(), oauthCheck(), access);
+api.post('/bind', bodyParser.json(), bind);
+
+api.post('/create', bodyParser.json(), create);
 
 api.get('/unbind', oauthCheck(), (req, res, next) => {
   req.model('wechat-util').unbindWechat(req.user._id)
   .then(() => {
     res.json({});
-    req.model('user-activity').createFromReq(req, C.USER_ACTIVITY.UNBIND_WECHAT);
+    // req.model('user-activity').createFromReq(req, C.USER_ACTIVITY.UNBIND_WECHAT);
   })
   .catch(next);
 });
@@ -69,12 +82,16 @@ api.post('/token', bodyParser.json(), wechatOauth.grant(), (req, res, next) => {
  * else (user)
  *  -> if (!userLogin) redirect to web app get accesstoken by authCode page with query string: {authCode: authCode}
  */
+
 function access(req, res, next) {
-  console.log(11111);
-  let gettingWechatOauth;
-  let whiteUrl;
   let { code, state } = req.query;
+  let wechatOAuthClient = getOAuthClient(req.model('redis'));
+  let oauthInfoErr = new Error('can not get access_token from wechat server');
+  if (!code) {
+    next(oauthInfoErr);
+  }
   let qs = '';
+  let whiteUrl;
   let whiteUrl_index;
   if (state) {
     qs = state.split('|').map((key, index) => {
@@ -87,80 +104,173 @@ function access(req, res, next) {
       else return `${key}&`;
     }).join('');
   }
-  let { random_token } = req.body;
-  let wechatOAuthClient = getOAuthClient(req.model('redis'));
-  let oauthInfoErr = new Error('can not get access_token from wechat server');
+  let deBUG = whiteUrl ? true : false;
   let wUtil = req.model('wechat-util');
-  if (random_token) {
-    gettingWechatOauth = wUtil.findWechatByRandomToken(random_token);
-  } else if (code) {
-    gettingWechatOauth = new Promise((resolve, reject) => {
-      wechatOAuthClient.getAccessToken(code, (err, result) => {
-        if (!result || !result.data) {
-          return reject(oauthInfoErr);
-        }
-        resolve(result.data);
-      });
-    });
-  } else {
-    next(oauthInfoErr);
-  }
-  gettingWechatOauth.then(wechat => {
-    if (!wechat) {
-      throw oauthInfoErr;
+  wechatOAuthClient.getAccessToken(code, (err, result) => {
+    if (!result || !result.data) {
+      next(oauthInfoErr);
     }
-    console.log(2222);
-    let { openid } = wechat;
-    let loginUser = getLoginUser(req);
-    let gettingUserWechat = loginUser ? wUtil.getUserWechat(loginUser._id) : Promise.resolve(null);
-    return gettingUserWechat.then(loginUserWechat => {
-      if (loginUserWechat) {
-        return res.redirect(urls.user(whiteUrl));
-      }
-      return wUtil.findUserByOpenid(openid)
-      .then(user => {
-        if (!user) {
-          console.log(3333);
-          if (loginUser) {
-            console.log(4444);
-            return wUtil.bindWechat(loginUser._id, wechat)
-            .then(() => {
-              res.json({});
-              req.model('user-activity').createFromReq(req, C.USER_ACTIVITY.BIND_WECHAT);
-            });
-          } else {
-            console.log(5555);
-            return wUtil.storeWechatOAuthAndGetRandomToken(wechat)
-            .then(oauthRandomToken => {
-              return wUtil.findWechatUserinfo(openid)
-              .then(userInfo => {
-                if (userInfo) {
-                  return res.redirect(urls.reg(oauthRandomToken, qs, whiteUrl));
-                }
-                return new Promise((resolve, reject) => {
-                  wechatOAuthClient.getUser(openid, (err, userInfo) => {
-                    if (err) {
-                      return reject(err);
-                    }
-                    resolve(userInfo);
-                  });
-                })
-                .then(userInfo => wUtil.storeWechatUserinfo(userInfo))
-                .then(() => res.redirect(urls.reg(oauthRandomToken, qs, whiteUrl)));
-              });
-            });
-          }
-        } else {
-          if (!loginUser) {
-            console.log(6666);
-            return wUtil.generateAuthCode(user._id)
-            .then(authCode => res.redirect(urls.token(authCode, qs, whiteUrl)));
-          }
+    let wechat = result.data;
+    let { openid, unionid } = wechat;
+    return wUtil.findUserByOpenidUnionid(openid, unionid)
+    .then(user => {
+      if (user) {
+        if (deBUG) {
+          qs ? (qs += `&user_id=${user._id}`) : qs = `user_id=${user._id}`;
         }
-      });
+        return wUtil.generateAuthCode(user._id)
+          .then(authCode => res.redirect((whiteUrl ? whiteUrl : mobileUrl) + 'account/login' + '?wechat_authcode=' + authCode + '&' + qs));
+      } else {
+        _storeNoaccountAuthcode(openid, unionid).then(authCode => {
+          res.redirect(mobileUrl + 'test' + '?wechat_authcode=' + authCode + '&' + qs);
+        });
+      }
     });
+  });
+}
+
+function bind(req, res, next) {
+  let { username, password, authCode } = req.body;
+  let criteria;
+  if (isMobile(username)) {
+    criteria = {mobile: username};
+  } else if (isEmail(username)) {
+    criteria = {email: username};
+  } else {
+    next(new ApiError(400, 'wrong_user_name'));
+  }
+  _checkAuthcode(authCode)
+  .then(doc => {
+    return _checkAccount(criteria, password, doc);
+  })
+  .then(user => {
+    res.json(user);
+  }).catch(next);
+}
+
+function create(req, res, next) {
+  let { mobile, code, password, authCode } = req.body;
+  let wechat;
+  if (!isMobile(mobile)) {
+    next(new ApiError(400, 'wrong_mobile'));
+  }
+  _checkAuthcode(authCode)
+  .then(doc => {
+    wechat = doc;
+    return req.model('account').verifySmsCode(mobile, code);
+  })
+  .then(() => {
+    return hashPassword(password);
+  })
+  .then(hash => {
+    return _createMobileAccount(hash, mobile, wechat);
+  })
+  .then(user => {
+    res.json(user);
   })
   .catch(next);
+}
+
+function _checkAuthcode(auth_code) {
+  return db.wechat.authcode.findOne({
+    auth_code,
+    used: false
+  })
+  .then(doc => {
+    if (!doc) {
+      throw new ApiError(400, 'auth_code_expire');
+    }
+    return doc;
+  });
+}
+
+function _storeNoaccountAuthcode(openid, unionid) {
+  return db.wechat.authcode.findOne({
+    unionid,
+  })
+  .then(doc => {
+    if (doc) {
+      return doc.auth_code;
+    } else {
+      return generateToken(20)
+      .then(authCode => {
+        db.wechat.authcode.insert({
+          openid,
+          unionid,
+          auth_code: authCode,
+          used: false
+        })
+        .then(() => {
+          return authCode;
+        });
+      });
+    }
+  });
+}
+
+function _checkAccount(account, password, doc) {
+  return db.user.findOne(
+    account,
+    {
+      password: 1,
+    }
+  )
+  .then(user => {
+    return comparePassword(password, user.password)
+    .then(result => {
+      if (result) {
+        return db.user.update({
+          _id: user._id
+        }, {
+          'wechat.openid': doc.openid,
+          'wechat.unionid': doc.unionid,
+          'wechat.auth.code': doc.auth_code,
+          'wechat.auth.expired': moment().add(config.get('wechat.auth_code_lifetime'), 'seconds').toDate()
+        })
+        .then(() => {
+          return user;
+        });
+      } else {
+        throw new ApiError(400, 'wrong_password');
+      }
+    });
+  });
+}
+
+function _createMobileAccount(hash, mobile, wechat) {
+  let doc = {
+    email: '',
+    email_verified: false,
+    mobile: mobile,
+    mobile_verified: true,
+    name: mobile.slice(-4),
+    description: '',
+    avatar: randomAvatar('user'),
+    password: hash,
+    birthdate: null,
+    address: {
+      country: '中国',
+      province: '',
+      city: '',
+      district: '',
+      address: '',
+    },
+    sex: null,
+    locale: 'zh-CN',
+    timezone: 'Asia/Shanghai',
+    activiated: true,
+    date_join: new Date,
+    date_create: new Date,
+    current_company: null,
+    wechat: {
+      'wechat.openid': wechat.openid,
+      'wechat.unionid': wechat.unionid,
+      'wechat.auth.code': wechat.auth_code,
+      'wechat.auth.expired': moment().add(config.get('wechat.auth_code_lifetime'), 'seconds').toDate()
+    }
+  };
+  let new_user = new db.user(doc);
+  return new_user.save();
 }
 
 function getLoginUser(req) {
