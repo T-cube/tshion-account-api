ae10b53abe13b20d63434b7a8adae973c912a208
xuezier <xzj15859722542@hotmail.com>
2018-06-27 14:30:52 +0700

    add post avatar upload api

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,21 +1,21 @@
 import _ from 'underscore';
 import express from 'express';
-import { ObjectId } from 'mongodb';
+import {ObjectId} from 'mongodb';
 import config from 'config';
 import compare from 'node-version-compare';
 import crypto from 'crypto';
 
 import C from 'lib/constants';
 import db from 'lib/database';
-import { ApiError } from 'lib/error';
-import { oauthCheck } from 'lib/middleware';
-import { upload, saveCdn, cropAvatar } from 'lib/upload';
-import { maskEmail, maskMobile, mapObjectIdToData } from 'lib/utils';
+import {ApiError} from 'lib/error';
+import {oauthCheck} from 'lib/middleware';
+import {upload, saveCdn, cropAvatar} from 'lib/upload';
+import {maskEmail, maskMobile, mapObjectIdToData} from 'lib/utils';
 import UserLevel from 'models/user-level';
 
-import { validate } from './schema';
+import {validate} from './schema';
 
-import { updateUserInfoCache } from 'lib/cache';
+import {updateUserInfoCache} from 'lib/cache';
 
 const version = require('../../../package.json').version;
 
@@ -37,7 +37,7 @@ const BASIC_FIELDS = {
   date_join: 1,
   current_company: 1,
   options: 1,
-  'wechat.openid': 1,
+  'wechat.openid': 1
 };
 
 /* users collection */
@@ -47,9 +47,14 @@ export default api;
 api.use(oauthCheck());
 
 api.get('/info', (req, res, next) => {
-  db.user.findOne({ _id: req.user._id }, BASIC_FIELDS)
+  db
+    .user
+    .findOne({
+      _id: req.user._id
+    }, BASIC_FIELDS)
     .then(data => {
-      new UserLevel(data).getLevelInfo()
+      new UserLevel(data)
+        .getLevelInfo()
         .then(levelInfo => {
           data.level_info = levelInfo;
           if (data.email) {
@@ -67,15 +72,17 @@ api.get('/info', (req, res, next) => {
 api.put('/info', (req, res, next) => {
   let data = req.body;
   validate('info', data);
-  db.user.update({
-    _id: req.user._id
-  }, {
-    $set: data
-  }).then(result => {
-    return updateUserInfoCache({ _id: req.user._id, data }).then(() => {
-      res.sendJson(result);
-    });
-  }).catch(next);
+  db
+    .user
+    .update({
+      _id: req.user._id
+    }, {$set: data})
+    .then(result => {
+      return updateUserInfoCache({_id: req.user._id, data}).then(() => {
+        res.sendJson(result);
+      });
+    })
+    .catch(next);
 });
 
 api.put('/settings', (req, res, next) => {
@@ -84,186 +91,221 @@ api.put('/settings', (req, res, next) => {
   if (!_.keys(data).length) {
     throw new ApiError(400, null, 'no setting provided!');
   }
-  db.user.update({
-    _id: req.user._id
-  }, {
-    $set: data,
-  }).then(() => res.sendJson(data)).catch(next);
-});
-
-api.put('/avatar', (req, res, next) => {
-  const data = {
-    avatar: cropAvatar(req),
-  };
-  db.user.update({
-    _id: req.user._id
-  }, {
-    $set: data,
-  }).then(() => {
-    return updateUserInfoCache({ _id: req.user._id, data }).then(() => {
-      res.sendJson(data);
-    });
-  }).catch(next);
-});
-
-api.put('/avatar/upload',
-  upload({ type: 'avatar' }).single('avatar'),
-  saveCdn('cdn-public'),
-  (req, res, next) => {
-    if (!req.file) {
-      throw new ApiError(400, 'file_type_error');
-    }
-    const data = {
-      avatar: cropAvatar(req),
-    };
-    db.user.update({
+  db
+    .user
+    .update({
       _id: req.user._id
-    }, {
-      $set: data
-    }).then(() => {
-      return updateUserInfoCache({ _id: req.user._id, data }).then(() => {
+    }, {$set: data})
+    .then(() => res.sendJson(data))
+    .catch(next);
+});
+
+let avatarFunc = (req, res, next) => {
+  const data = {
+    avatar: cropAvatar(req)
+  };
+  db
+    .user
+    .update({
+      _id: req.user._id
+    }, {$set: data})
+    .then(() => {
+      return updateUserInfoCache({_id: req.user._id, data}).then(() => {
         res.sendJson(data);
       });
-    }).catch(next);
-  });
+    })
+    .catch(next);
+};
+
+api.post('/avatar', avatarFunc);
+
+api.put('/avatar', avatarFunc);
+
+let uploadFunc = (req, res, next) => {
+  if (!req.file) {
+    throw new ApiError(400, 'file_type_error');
+  }
+  const data = {
+    avatar: cropAvatar(req)
+  };
+  db
+    .user
+    .update({
+      _id: req.user._id
+    }, {$set: data})
+    .then(() => {
+      return updateUserInfoCache({_id: req.user._id, data}).then(() => {
+        res.sendJson(data);
+      });
+    })
+    .catch(next);
+};
+
+api.post('/avatar/upload', upload({type: 'avatar'}).single('avatar'), saveCdn('cdn-public'), uploadFunc);
+
+api.put('/avatar/upload', upload({type: 'avatar'}).single('avatar'), saveCdn('cdn-public'), uploadFunc);
 
 api.get('/guide', (req, res, next) => {
-  db.user.guide.findOne({
-    _id: req.user._id
-  }).then(user_guide => {
-    let new_user, guide;
-    if (!guide) {
-      new_user = 1;
-      guide = 1;
-    } else {
-      new_user = 0;
-      guide = compare(version, user_guide.version);
-    }
-    return res.sendJson({
-      version,
-      new_user,
-      guide,
-    });
-  }).catch(next);
+  db
+    .user
+    .guide
+    .findOne({_id: req.user._id})
+    .then(user_guide => {
+      let new_user,
+        guide;
+      if (!guide) {
+        new_user = 1;
+        guide = 1;
+      } else {
+        new_user = 0;
+        guide = compare(version, user_guide.version);
+      }
+      return res.sendJson({version, new_user, guide});
+    })
+    .catch(next);
 });
 
 api.put('/guide', (req, res, next) => {
-  db.user.guide.update({
-    _id: req.user._id,
-  }, {
-    $set: { version }
-  }, {
-    upsert: true
-  }).then(() => res.sendJson(200)).catch(next);
+  db
+    .user
+    .guide
+    .update({
+      _id: req.user._id
+    }, {
+      $set: {
+        version
+      }
+    }, {upsert: true})
+    .then(() => res.sendJson(200))
+    .catch(next);
 });
 
 api.get('/guide/new', (req, res, next) => {
-  db.guide.get({
-    version: 0
-  }).then(doc => res.sendJson(doc)).catch(next);
+  db
+    .guide
+    .get({version: 0})
+    .then(doc => res.sendJson(doc))
+    .catch(next);
 });
 
 api.get('/guide/version/:version', (req, res, next) => {
-  db.guide.get({
-    version: req.params.version
-  }).then(doc => res.sendJson(doc)).catch(next);
+  db
+    .guide
+    .get({version: req.params.version})
+    .then(doc => res.sendJson(doc))
+    .catch(next);
 });
 
 api.post('/guide', (req, res, next) => {
   let data = req.body;
-  db.guide.insert(data).then(() => res.sendJson(200)).catch(next);
+  db
+    .guide
+    .insert(data)
+    .then(() => res.sendJson(200))
+    .catch(next);
 });
 
 api.get('/invite', (req, res, next) => {
   let redis = req.model('redis');
   let timetemp = new Date().getTime();
-  db.user.findOne({
-    _id: req.user._id
-  }).then(doc => {
-    let user_id = req.user._id.toString();
-    let company_id = doc.current_company.toString();
+  db
+    .user
+    .findOne({_id: req.user._id})
+    .then(doc => {
+      let user_id = req
+        .user
+        ._id
+        .toString();
+      let company_id = doc
+        .current_company
+        .toString();
 
-  }).catch(next);
+    })
+    .catch(next);
 });
 
 api.post('/invitation', (req, res, next) => {
   let company_id = ObjectId(req.body.company_id);
-  db.company.findOne({
-    _id: company_id
-  }).then(doc => {
-    if (!doc) {
-      res.sendJson(...req.body);
-    }
-    if (_.some(doc.members, m => m._id.equals(req.user._id) && m.status == 'normal')) {
-      res.sendJson({ _id: doc._id, name: doc.name });
-    } else if (_.some(doc.members, m => m._id.equals(req.user._id))) {
-      return Promise.all([
-        db.user.update({
-          _id: req.user._id
-        }, {
-          $addToSet: {
-            companies: company_id
-          }
-        }),
-        db.company.update({
-          _id: doc._id,
-          'members._id': req.user._id
-        }, {
-          $set: {
-            'members.$.status': C.COMPANY_MEMBER_STATUS.NORMAL
-          },
-          $addToSet: {
-            'structure.members': {
+  db
+    .company
+    .findOne({_id: company_id})
+    .then(doc => {
+      if (!doc) {
+        res.sendJson(...req.body);
+      }
+      if (_.some(doc.members, m => m._id.equals(req.user._id) && m.status == 'normal')) {
+        res.sendJson({_id: doc._id, name: doc.name});
+      } else if (_.some(doc.members, m => m._id.equals(req.user._id))) {
+        return Promise.all([
+          db
+            .user
+            .update({
               _id: req.user._id
-            }
-          }
-        }),
-        req.model('activity').insert({
-          creator: req.user._id,
-          company: company_id,
-          action: C.ACTIVITY_ACTION.JOIN,
-          target_type: C.OBJECT_TYPE.COMPANY,
-        })
-      ]).then(() => {
-        res.sendJson({ _id: doc._id, name: doc.name });
-      });
-    } else {
-      return Promise.all([
-        db.user.update({
-          _id: req.user._id
-        }, {
-          $addToSet: {
-            companies: company_id
-          }
-        }),
-        db.company.update({
-          _id: company_id,
-        }, {
-          $addToSet: {
-            members: {
-              _id: req.user._id,
-              name: req.user.name,
-              email: req.user.email,
-              mobile: req.user.mobile,
-              sex: null,
-              status: C.COMPANY_MEMBER_STATUS.NORMAL,
-              type: C.COMPANY_MEMBER_TYPE.NORMAL,
-              address: ''
-            },
-            'structure.members': {
+            }, {
+              $addToSet: {
+                companies: company_id
+              }
+            }),
+          db
+            .company
+            .update({
+              _id: doc._id,
+              'members._id': req.user._id
+            }, {
+              $set: {
+                'members.$.status': C.COMPANY_MEMBER_STATUS.NORMAL
+              },
+              $addToSet: {
+                'structure.members': {
+                  _id: req.user._id
+                }
+              }
+            }),
+          req
+            .model('activity')
+            .insert({creator: req.user._id, company: company_id, action: C.ACTIVITY_ACTION.JOIN, target_type: C.OBJECT_TYPE.COMPANY})
+        ]).then(() => {
+          res.sendJson({_id: doc._id, name: doc.name});
+        });
+      } else {
+        return Promise.all([
+          db
+            .user
+            .update({
               _id: req.user._id
-            }
-          }
-        }),
-        req.model('activity').insert({
-          creator: req.user._id,
-          company: company_id,
-          action: C.ACTIVITY_ACTION.JOIN,
-          target_type: C.OBJECT_TYPE.COMPANY,
-        })
-      ]).then(() => {
-        res.sendJson({ _id: doc._id, name: doc.name });
-      });
-    }
-  }).catch(next);
+            }, {
+              $addToSet: {
+                companies: company_id
+              }
+            }),
+          db
+            .company
+            .update({
+              _id: company_id
+            }, {
+              $addToSet: {
+                members: {
+                  _id: req.user._id,
+                  name: req.user.name,
+                  email: req.user.email,
+                  mobile: req.user.mobile,
+                  sex: null,
+                  status: C.COMPANY_MEMBER_STATUS.NORMAL,
+                  type: C.COMPANY_MEMBER_TYPE.NORMAL,
+                  address: ''
+                },
+                'structure.members': {
+                  _id: req.user._id
+                }
+              }
+            }),
+          req
+            .model('activity')
+            .insert({creator: req.user._id, company: company_id, action: C.ACTIVITY_ACTION.JOIN, target_type: C.OBJECT_TYPE.COMPANY})
+        ]).then(() => {
+          res.sendJson({_id: doc._id, name: doc.name});
+        });
+      }
+    })
+    .catch(next);
 });
